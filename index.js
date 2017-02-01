de8842b71d6b4492539a80cebd8eee3698ed62ac
Enrique Barragan <ebarragan1997@gmail.com>
2017-02-01 15:06:46 -0600

    changed language

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function(bp) {
   bp.middlewares.load();
-  bp.hear(/hello/i, (event, next) => { // We use a regex instead of a hardcoded string
+  bp.hear(/hola/i, (event, next) => { // We use a regex instead of a hardcoded string
     const first_name = event.user.first_name
 
-    bp.messenger.sendText(event.user.id, 'Hello, ' + first_name, { typing: true })
+    bp.messenger.sendText(event.user.id, 'Hola, ' + first_name, { typing: true })
   });
 }
