4a7ade08fd097ee3df7a00a242fcbc3c27f3c7e8
Enrique Barragan <ebarragan1997@gmail.com>
2017-02-16 16:30:13 -0600

    Agregar comedor y hamburguesas

--- bot/comida.js
+++ bot/comida.js
@@ -110,6 +110,28 @@ module.exports = function (bp) {
       bp.messenger.sendTemplate(event.user.id, starbucksPayload);
     });
 
+    bp.hear(/comedor/i, (event, next) => {
+      bp.messenger.sendText(event.user.id, 'Abierto de 07:00 a 16:30 hrs de lunes a viernes, comida de cafetería.')
+      const cedesComedorPayload = {
+        template_type: "generic",
+        elements: [
+          {
+            "title":"Comedor Estudiantil",
+            "image_url":"https://maps.googleapis.com/maps/api/staticmap?center=25.652733567570262,-100.29206648468971&zoom=17&scale=false&size=600x300&maptype=roadmap&key=AIzaSyBOYAfqdxetXof7_OcKXUs-mwr6q9IX2Ck&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C25.652733567570262,-100.29206648468971",
+            subtitle:"Se encuentra cerca de CEDES, pasando el puente subterráneo que se encuentra a un lado de rectoría.",
+            buttons: [
+              {
+                  type: "web_url",
+                  url: "https://goo.gl/maps/YfeuZqpH1ET2",
+                  title: "Direcciones"
+              }
+            ]
+          }
+        ]
+      }
+      bp.messenger.sendTemplate(event.user.id, cedesComedorPayload);
+    });
+
     bp.hear(/foodbox/i, (event,next) => {
       bp.messenger.sendText(event.user.id, 'Hamburguesas, alitas y más. Abierto de 9:00 a 18:00 entre semana, los sábados de 8:00 a 16:00.')
       const centralesPayload = {
@@ -123,6 +145,28 @@ module.exports = function (bp) {
               {
                   type: "web_url",
                   url: "https://goo.gl/maps/KrXCFwqPEqP2",
+                  title: "Direcciones"
+              }
+            ]
+          }
+        ]
+      }
+      bp.messenger.sendTemplate(event.user.id, centralesPayload)
+    });
+
+    bp.hear(/el camino/i, (event,next) => {
+      bp.messenger.sendText(event.user.id, 'Hamburguesas. Abierto Martes y Jueves de 12:00 a 14:30 hrs.')
+      const centralesPayload = {
+        template_type: "generic",
+        elements: [
+          {
+            "title":"El Camino Foodtruck",
+            "image_url":"https://maps.googleapis.com/maps/api/staticmap?center=25.652651361130005,-100.28976514935493&zoom=18&scale=false&size=600x300&maptype=roadmap&key=AIzaSyBOYAfqdxetXof7_OcKXUs-mwr6q9IX2Ck&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C25.652651361130005,-100.28976514935493",
+            "subtitle":"Se encuentra a un lado del Centro de Biotecnología",
+            buttons: [
+              {
+                  type: "web_url",
+                  url: "https://goo.gl/maps/UDq9p2YkE522",
                   title: "Direcciones"
               }
             ]
@@ -190,77 +234,46 @@ module.exports = function (bp) {
 
     bp.hear(/cafeteria/i, (event, next) => {
       const cafeteriaPayload = {
-            template_type: "list",
-            top_element_style: "compact",
-            elements: [
-                {
-                    title: "Centrales",
-                    image_url: "https://maps.googleapis.com/maps/api/staticmap?center=25.6514569435194,-100.28904765844345&zoom=19&scale=false&size=600x300&maptype=roadmap&key=AIzaSyBOYAfqdxetXof7_OcKXUs-mwr6q9IX2Ck&format=png&visual_refresh=true",
-                    subtitle: "Centrales se encuentra entre aulas 1 y 2, frente al domo acuático.",
-                    default_action: {
-                        type: "web_url",
-                        url: "",
-                        messenger_extensions: true,
-                        webview_height_ratio: "tall",
-                        fallback_url: "https://goo.gl/maps/KrXCFwqPEqP2"
-                    },
-                    buttons: [
-                        {
-                            title: "Direcciones",
-                            type: "web_url",
-                            url: "https://goo.gl/maps/KrXCFwqPEqP2",
-                            messenger_extensions: true,
-                            webview_height_ratio: "tall",
-                            fallback_url: "https://goo.gl/maps/KrXCFwqPEqP2"
-                        }
-                    ]
+        template_type: "button",
+            text: "Tenemos estas opciones de cafetería",
+            buttons: [
+              {
+                  type: "postback",
+                  payload: "CENTRALES",
+                  title: "Centrales"
               },
               {
-                  title: "Centrales",
-                  image_url: "https://maps.googleapis.com/maps/api/staticmap?center=25.6514569435194,-100.28904765844345&zoom=19&scale=false&size=600x300&maptype=roadmap&key=AIzaSyBOYAfqdxetXof7_OcKXUs-mwr6q9IX2Ck&format=png&visual_refresh=true",
-                  subtitle: "Centrales se encuentra entre aulas 1 y 2, frente al domo acuático.",
-                  default_action: {
-                      type: "web_url",
-                      url: "",
-                      messenger_extensions: true,
-                      webview_height_ratio: "tall",
-                      fallback_url: "https://goo.gl/maps/KrXCFwqPEqP2"
-                  },
-                  buttons: [
-                      {
-                          title: "Direcciones",
-                          type: "web_url",
-                          url: "https://goo.gl/maps/KrXCFwqPEqP2",
-                          messenger_extensions: true,
-                          webview_height_ratio: "tall",
-                          fallback_url: "https://goo.gl/maps/KrXCFwqPEqP2"
-                      }
-                  ]
-            },
-            {
-                title: "Centrales",
-                image_url: "https://maps.googleapis.com/maps/api/staticmap?center=25.6514569435194,-100.28904765844345&zoom=19&scale=false&size=600x300&maptype=roadmap&key=AIzaSyBOYAfqdxetXof7_OcKXUs-mwr6q9IX2Ck&format=png&visual_refresh=true",
-                subtitle: "Centrales se encuentra entre aulas 1 y 2, frente al domo acuático.",
-                default_action: {
-                    type: "web_url",
-                    url: "",
-                    messenger_extensions: true,
-                    webview_height_ratio: "tall",
-                    fallback_url: "https://goo.gl/maps/KrXCFwqPEqP2"
-                },
-                buttons: [
-                    {
-                        title: "Direcciones",
-                        type: "web_url",
-                        url: "https://goo.gl/maps/KrXCFwqPEqP2",
-                        messenger_extensions: true,
-                        webview_height_ratio: "tall",
-                        fallback_url: "https://goo.gl/maps/KrXCFwqPEqP2"
-                    }
-                ]
-          }
-            ]
-          }
-          bp.messenger.sendTemplate(event.user.id, cafeteriaPayload);
+                  type: "postback",
+                  payload: "JUBILEO",
+                  title: "Jubileo"
+              },
+              {
+                  type: "postback",
+                  payload: "COMEDOR",
+                  title: "Comedor Estudiantil"
+              }
+        ]
+      }
+      bp.messenger.sendTemplate(event.user.id, cafeteriaPayload);
+    });
+
+    bp.hear(/hamburguesa/i, (event, next) => {
+      const hamburguesasPayload = {
+        template_type: "button",
+            text: "Tenemos estas opciones de Hamburguesas",
+            buttons: [
+              {
+                  type: "postback",
+                  payload: "FOODBOX",
+                  title: "Foodbox"
+              },
+              {
+                  type: "postback",
+                  payload: "EL CAMINO",
+                  title: "El Camino Foodtruck"
+              }
+        ]
+      }
+      bp.messenger.sendTemplate(event.user.id, hamburguesasPayload);
     });
 };
